9fd2a790b9fee714fe4c6699ba84a4ed302c20c8
jacmilski <jacmilski@gmail.com>
2022-12-07 13:37:27 +0100

    modified query in NewsLayout component

--- src/layouts/news.jsx
+++ src/layouts/news.jsx
@@ -5,12 +5,19 @@ import { Link, graphql } from 'gatsby';
 
 const NewsLayout = ({ data }) => {
 
+  console.log(data.datoCmsNews.newsBlocks[0].newsContentPicture.url
+    )
+
+  const image = data.datoCmsNews.newsBlocks[0].newsContentPicture.url;
+
     return(
         <NewsWrapper>
           <Link to="/">wróć</Link>
           <h2>{data.datoCmsNews.newsTitle}</h2>
           <p>{data.datoCmsNews.newsDate}</p>
+          {/* <GatsbyImage image={image} /> */}
           <p>{data.datoCmsNews.newsParagraph}</p>
+          <img src={image} alt="" width="450"/>
         </NewsWrapper>
     )
 }
@@ -23,6 +30,12 @@ export const query = graphql`
       newsTitle
       newsDate(formatString: "DD-MM-YYYY")
       newsParagraph
+      newsBlocks {
+        id
+        newsContentPicture {
+          url
+        }
+      }
     }
   }
 `
