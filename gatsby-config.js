38e996d0d3ac900c155c59cb72804b826308df42
jacmilski <jacmilski@gmail.com>
2023-06-09 19:42:33 +0200

    Added plugins for gtag, robots.txt and sitemap

--- gatsby-config.js
+++ gatsby-config.js
@@ -21,6 +21,7 @@ module.exports = {
     phoneDirector: `798527256`,
     emailDirector: `mierzejewskiandrzej44@gmail.com`,
     author: `Jacek Michalski`,
+    siteUrl: `https://szkolamuzyczna.bartoszyce.pl`,
   },
   plugins: [
     `gatsby-plugin-image`,
@@ -108,6 +109,46 @@ module.exports = {
       options: {
         name: `avatars`,
         path: `${__dirname}/src/assets/avatars`
+      },
+    },
+    `gatsby-plugin-sitemap`,
+    {
+      resolve: 'gatsby-plugin-robots-txt',
+      options: {
+        host: `https://szkolamuzyczna.bartoszyce.pl`,
+        sitemap: `https://szkolamuzyczna.bartoszyce.pl/sitemap.xml`,
+        policy: [{userAgent: '*', allow: '/'}]
+      }
+    },
+    {
+      resolve: `gatsby-plugin-google-gtag`,
+      // G-MFLNNV8PDT
+      options: {
+        // You can add multiple tracking ids and a pageview event will be fired for all of them.
+        trackingIds: [
+          `G-MFLNNV8PDT`,
+          `GA-TRACKING_ID`, // Google Analytics / GA
+          `AW-CONVERSION_ID`, // Google Ads / Adwords / AW
+          `DC-FLOODIGHT_ID`, // Marketing Platform advertising products (Display & Video 360, Search Ads 360, and Campaign Manager)
+        ],
+        // This object gets passed directly to the gtag config command
+        // This config will be shared across all trackingIds
+        gtagConfig: {
+          optimize_id: "OPT_CONTAINER_ID",
+          anonymize_ip: true,
+          cookie_expires: 0,
+        },
+        // This object is used for configuration specific to this plugin
+        pluginConfig: {
+          // Puts tracking script in the head instead of the body
+          head: true,
+          // Setting this parameter is also optional
+          respectDNT: true,
+          // Avoids sending pageview hits from custom paths
+          exclude: ["/preview/**", "/do-not-track/me/too/"],
+          // Defaults to https://www.googletagmanager.com
+          origin: "YOUR_SELF_HOSTED_ORIGIN",
+        },
       },
     },
     `gatsby-transformer-sharp`,
